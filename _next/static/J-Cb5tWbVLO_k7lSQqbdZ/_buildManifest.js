d01468ef6433f37afff50bc5d90dc15deef53dc9
Mridul Chopra <chomy077@mymail.unisa.edu.au>
2026-02-17 23:05:09 +1030

    Updates

--- _next/static/J-Cb5tWbVLO_k7lSQqbdZ/_buildManifest.js
+++ _next/static/J-Cb5tWbVLO_k7lSQqbdZ/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/mridul-portfolio//_next/:path+",
+        "destination": "/mridul-portfolio/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
